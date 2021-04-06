32d69d29a4bfa928532d258fef3d665081d4a08b
nurcholismajid <boyzitakazi@gmail.com>
2021-04-06 19:30:19 +0700

    DHCP Server done

--- build.py
+++ build.py
@@ -31,6 +31,15 @@ def show_menu():
     menu = int(input("Pilih Menu : "))
     return menu
 
+# menu konfigurasi
+def configurate_menu(service):
+    os.system("clear")
+    console.print(f" :ramen: [italic purple]Configuration[/italic purple] & [italic yellow]Reconfiguration[/italic yellow] [bold green]{service}[/bold green]")
+    console.print(" -------------------------------------------------------------------------")
+    console.print(" :memo:", end="")
+    recon = int(input(" Menu : (1). configure (2). setting ke awal (3). reconfigure (4). back: "))
+    return recon
+
 # Konfigurasi IP address
 def ip_addr_configure():
     # backup file
@@ -58,7 +67,9 @@ def ip_addr_configure():
     os.system('ip addr');input("Enter untuk kembali ke menu...")
     os.system('clear')
 
+# ip address reconfigure
 def ip_addr_reconfigure():
+    # backup file
     backup_folder = f"{dir}/.backup"
     os.system(f"cp {backup_folder}/interfaces /etc/network/interfaces")
     
@@ -66,6 +77,52 @@ def ip_addr_reconfigure():
     os.system('/etc/init.d/networking restart')
     os.system(f'ifup {interfaces}')
     os.system('service networking restart');input("Enter untuk kembali ke menu...")
+
+# DHCP Configure
+def dhcp_server_configure():
+    # backup files
+    backup_folder = f"{dir}/.backup"
+    os.system(f"cp /etc/dhcp/dhcpd.conf {backup_folder}")
+    os.system(f"cp /etc/default/isc-dhcp-server {backup_folder}")
+
+    # User input
+    interfaces = input("Masukan Nama INTERFACES : ")
+    subnet = input("Masukan SUBNET : ")
+    netmask = input("Masukan NETMASK : ")
+    ip_range = input("Masukan IP RANGE : ")
+    dns = input("Masukan DNS : ")
+    domain_name = input("Masukan DOMAIN NAME : ")
+    routers = input("Masukan ROUTERS : ")
+    broadcast = input("Masukan BROADCAST : ")
+
+    # configurate
+    os.system(f'sed -i "s/# subnet 10.5.5.0 netmask 255.255.255.224/subnet $SUBNET netmask {netmask}/g" /etc/dhcp/dhcpd.conf')
+    os.system(f'sed -i "s/#  range 10.5.5.26 10.5.5.30/  range {ip_range}/g" /etc/dhcp/dhcpd.conf')
+    os.system(f'sed -i "s/#  option domain-name-servers ns1.internal.example.org/  option domain-name-servers {dns}/g" /etc/dhcp/dhcpd.conf')
+    os.system(f'sed -i "s/#  option domain-name \"internal.example.org\"/  option domain-name \"{domain_name}\"/g" /etc/dhcp/dhcpd.conf')
+    os.system(f'sed -i "s/#  option routers 10.5.5.1/  option routers {routers}/g" /etc/dhcp/dhcpd.conf')
+    os.system(f'sed -i "s/#  option broadcast-address 10.5.5.31/  option broadcast-address {broadcast}/g" /etc/dhcp/dhcpd.conf')
+    os.system(f'sed -i "s/#  default-lease-time 600/  default-lease-time 600/g" /etc/dhcp/dhcpd.conf')
+    os.system(f'sed -i "s/#  max-lease-time 7200/  max-lease-time 7200/g" /etc/dhcp/dhcpd.conf')
+    os.system(f'sed -i "s/#}/}/g" /etc/dhcp/dhcpd.conf')
+    
+    # Setting Interfaces DHCP
+    os.system(f'sed -i "s/INTERFACESv4=\"\"/INTERFACESv4=\"{interfaces}\"/g" /etc/default/isc-dhcp-server')
+
+    # restarting
+    os.system("systemctl restart isc-dhcp-server")
+    os.system("systemctl status isc-dhcp-server")
+
+# DHCP reconfigure
+def dhcp_server_reconfigure():
+    # backup file
+    backup_folder = f"{dir}/.backup"
+    os.system(f"cp {backup_folder}/dhcpd.conf /etc/dhcp/dhcpd.conf")
+    os.system(f"cp {backup_folder}/isc-dhcp-server /etc/default/isc-dhcp-server")
+    
+    # restarting network
+    os.system("systemctl restart isc-dhcp-server")
+    os.system("systemctl status isc-dhcp-server");input("Enter untuk kembali ke menu...")
 
 if __name__ == "__main__":
     while(True):
@@ -75,21 +132,25 @@ if __name__ == "__main__":
         menu = show_menu()
 
         if (menu == 1):
-            os.system("clear")
-            console.print(" :ramen: [italic purple]Configuration[/italic purple] & [italic yellow]Reconfiguration[/italic yellow] [bold green]IP Address[/bold green]")
-            console.print(" -------------------------------------------------------------------------")
-            console.print(" :memo:", end="")
-            recon = int(input(" Menu : (1). configure (2). setting ke awal (3). reconfigure (4). back: "))
-            if (recon == 1):
+            menu_conf = configurate_menu()
+            if (menu_conf == 1):
                 ip_addr_configure() # configure
-            elif (recon == 2):
+            elif (menu_conf == 2):
                 ip_addr_reconfigure() # reconfigure
-            elif (recon == 3):
+            elif (menu_conf == 3):
                 ip_addr_reconfigure # reconfigure
                 ip_addr_configure() # configure
 
         elif (menu == 2):
-            pass
+            menu_conf = configurate_menu()
+            if (menu_conf == 1):
+                dhcp_server_configure() # configure
+            elif (menu_conf == 2):
+                dhcp_server_reconfigure() # reconfigure
+            elif (menu_conf == 3):
+                dhcp_server_reconfigure # reconfigure
+                dhcp_server_configure() # configure
+
         elif (menu == 3):
             pass
         elif (menu == 4):
