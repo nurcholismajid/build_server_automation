4412e874305e6a771595e9717e00d656a020f00a
Nurcholis Majid <80020809+nurcholismajid@users.noreply.github.com>
2022-03-01 17:59:01 +0700

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -1,2 +1,3 @@
 # Requiremnts
-apt-get install isc-dhcp-server bind9 dnsutils apache2 php7.0 mysql-server phpmyadmin openssh-server vsftpd -y
+# apt-get install isc-dhcp-server bind9 dnsutils apache2 php7.0 mysql-server phpmyadmin openssh-server vsftpd -y
+apt-get install git python3 python3-pip isc-dhcp-server bind9 dnsutils apache2 php7.0 mysql-server phpmyadmin openssh-server vsftpd ufw postfix courier-imap courier-pop roundcube -y
